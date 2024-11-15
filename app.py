b65c6a184ab99302d206846570132fb9f88b48c7
IT'S ME SRINU <103115872+Itsmesrinu@users.noreply.github.com>
2024-11-16 05:21:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,26 @@ from pdfcontextbot import pdfcontextbot
 from object_det import object_det
 
 def main():
+    # Set page configuration
     st.set_page_config(layout='wide')
+
+    # Custom CSS for dark theme
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-color: #1e1e1e; /* Dark background */
+            color: white; /* White text */
+        }
+        .sidebar .sidebar-content {
+            background-color: #2b2b2b; /* Dark sidebar */
+            color: white; /* White text in sidebar */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     st.sidebar.image(r"logomedi.png")
     navigation = st.sidebar.selectbox("Menu", ["HOME", "ABOUT", "PDF MEDICAL REPORT ANALYSIS", "PDF CONTEXT MEDICAL CHAT BOT", "CSV PATIENT DATA ANALYSIS", "MEDICAL IMAGE ANALYSIS", "CHAT WITH MEDICARE BOT"])
 
